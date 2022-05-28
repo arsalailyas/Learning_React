48bba7ec802e00e4ae26f4bf2a40e2641a6d9fc7
arsalailyas <arsalailyas@gmail.com>
2022-05-28 13:44:09 -0700

    Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      
+    <div className='container py-5'>
+
+      <h1 className='display-3 text-center py-5'>You Clicked <span className='text-danger fw-bold'> {count} </span> Times</h1>
+
+      <div className='d-flex justify-content-center'>
+        <button className='btn btn-success shadow-none' onClick={() => setCount(count + 1)}>Click Me</button>
+      </div>
+
     </div>
   );
 }
