c172435a2eefcadd77a6554dc48b3d521ce88643
arsalailyas <arsalailyas@gmail.com>
2022-06-04 03:58:33 -0700

    Upaded

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
       <h1 className='display-3 text-center py-5'>You Clicked <span className='text-danger fw-bold'> {count} </span> Times</h1>
 
       <div className='d-flex justify-content-center'>
-        <button className='btn btn-success shadow-none' onClick={() => setCount(count + 1)}>Click Me</button>
+        <button className='btn btn-primary' onClick={() => setCount(count + 1)}>Click Me</button>
       </div>
-
+      
     </div>
   );
 }
